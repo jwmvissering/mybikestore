64f48d299f2bf0b08ba10a005c0e70b34365582b
Jamon <jamonvissering@gmail.com>
2023-11-22 14:24:59 -0700

    Added bike detail page and worked on styling of multiple components

--- mybikestore-web/src/app/item-details/item-details.component.ts
+++ mybikestore-web/src/app/item-details/item-details.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink} from '@angular/router';
 import {OverviewFilterComponent} from "../overview/overview-filter/overview-filter.component";
 import {OverviewItemComponent} from "../overview/overview-item/overview-item.component";
 import {BackButtonComponent} from "../shared/components/back-button/back-button.component";
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-item-details',
   standalone: true,
-  imports: [CommonModule, RouterLink, OverviewFilterComponent, OverviewItemComponent, BackButtonComponent],
+  imports: [CommonModule, RouterLink, OverviewFilterComponent, OverviewItemComponent, BackButtonComponent, MatTooltipModule],
   templateUrl: './item-details.component.html',
   styleUrl: './item-details.component.scss'
 })
--- mybikestore-web/src/app/overview/overview-item/overview-item.component.ts
+++ mybikestore-web/src/app/overview/overview-item/overview-item.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-overview-item',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatTooltipModule],
   templateUrl: './overview-item.component.html',
   styleUrl: './overview-item.component.scss'
 })
